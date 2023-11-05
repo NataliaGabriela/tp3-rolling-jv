804b44fbe86e137f1753c59bfddb5a6b66c9657e
Natalia Lopez <natigab.24@gmail.com>
2023-11-05 00:17:46 -0600

    primera parte del ejercicio 3, sólo contiene la suma de aleatorios

---
+++ js/ejercicio3.js
@@ -0,0 +1,15 @@
+const max = 6;
+const min = 1;
+const arraySuma = [];
+
+for (let i = 0; i < 50; i++) {
+  
+    let sumaDeAleatorios = 0;
+    for (let lanzamientos = 0; lanzamientos < 2; lanzamientos++) {
+      sumaDeAleatorios += Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    arraySuma[i];
+  
+  }
+
+
